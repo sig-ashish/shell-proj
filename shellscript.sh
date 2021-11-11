a69da0ee7d03fa0992ec945b36f233d3730770cd
shreedhar-sigmoid <91470568+shreedhar-sigmoid@users.noreply.github.com>
2021-11-11 19:43:26 +0530

    Update shellscript.sh

--- shellscript.sh
+++ shellscript.sh
@@ -1,76 +1,71 @@
 #!/bin/bash
 
 
-COMPONENT_LIST=( INGESTOR JOINER WRANGLER VALIDATOR )
-echo "Please select the component name:"
-select COMPONENT_NAME in "${COMPONENT_LIST[@]}"; do
-    if [[ " ${COMPONENT_LIST[*]} " == *" $COMPONENT_NAME "* ]]; then
-        echo "You have selected$COMPONENT_NAME"
-        break
-    else
-        echo "invalid selection: $REPLY"
-        echo "Please select the valid component name"
-    fi
-done
-VIEW_LIST=( Auction Bid )
-echo "Please select the view:"
-select VIEW in "${VIEW_LIST[@]}"; do
-    if [[ " ${VIEW_LIST[*]} " == *" $VIEW "* ]]; then
-        echo "you have selected $VIEW"
-        break
-    else
-        echo "invalid selection: $REPLY"
-        echo "Please select the valid view"
-    fi
-done
-SCALE_LIST=( LOW MID HIGH )
-echo "Please select the scale:"
-select SCALE in "${SCALE_LIST[@]}"; do
-    if [[ " ${SCALE_LIST[*]} " == *" $SCALE "* ]]; then
-        echo "you have selected $SCALE"
-        break
-    else
-        echo "invalid selection: $REPLY"
-        echo "Please select the valid scale"
-    fi
-done
-COUNT_LIST=( 1 2 3 4 5 6 7 8 9)
-echo "Please select the count:"
-select COUNT in "${COUNT_LIST[@]}"; do
-    if [[ " ${COUNT_LIST[*]} " == *" $COUNT "* ]]; then
-        echo "you have selected $COUNT"
-        break
-    else
-        echo "invalid selection: $REPLY"
-        echo "Please select the valid count"
-    fi
-done
 
-if [ "$VIEW" == "Auction" ];
+function choose(){
+list=("${!1}")
+PS3="$2"
+select value in ${list[@]}; do
+    if [[ " ${list[*]} " =~ " $value " ]]; then
+        echo "You have selected: $value" >&2
+        echo "$value"
+        break
+    else
+        echo "   $REPLY is invalid selection, please select the vaild option" >&2
+    fi
+    
+done
+}
+
+component_array=(INGESTOR JOINER WRANGLER VALIDATOR)
+component_ps3="Please select the component name: "
+
+view_array=(Auction Bid)
+view_ps3="Please select the view: "
+
+scale_array=(LOW MID HIGH)
+scale_ps3="Please select the scale: "
+
+count_array=({1..9})
+count_ps3="Please select the count: "
+
+
+component=$( choose component_array[@] "$component_ps3" )
+view=$( choose view_array[@] "$view_ps3" )
+scale=$( choose scale_array[@] "$scale_ps3" )
+count=$( choose count_array[@] "$count_ps3" )
+
+
+if [ "$view" == "Auction" ];
     then    
-        if cat sig.conf | grep -n -v "vdopiasample-bid" | grep -n -q "$COMPONENT_NAME" ;
+        if cat sig.conf | grep -n -v "vdopiasample-bid" | grep -n -q "$component" ;
            then
-                LINE_NUMBER=$( cat sig.conf | grep -n -v "vdopiasample-bid" | grep  "$COMPONENT_NAME" | awk -F ':' '{print $1}')
-                DEFAULT_SCALE=$( cat sig.conf | grep -n -v "vdopiasample-bid" | grep -n "$COMPONENT_NAME" | awk -F ';' '{print $2}'  )
-                DEFAULT_COUNT=$( cat sig.conf | grep -n -v "vdopiasample-bid" | grep -n "$COMPONENT_NAME" | awk -F '=' '{print $2}'  )
-                sed -i "${LINE_NUMBER}s/${DEFAULT_SCALE}/${SCALE}/" sig.conf
-                sed -i "${LINE_NUMBER}s/${DEFAULT_COUNT}/${COUNT}/" sig.conf
-
+                line_number=$( cat sig.conf | grep -n -v "vdopiasample-bid" | grep  "$component" | awk -F ':' '{print $1}')
+                default_scale=$( cat sig.conf | grep -n -v "vdopiasample-bid" | grep -n "$component" | awk -F ';' '{print $2}'  )
+                default_count=$( cat sig.conf | grep -n -v "vdopiasample-bid" | grep -n "$component" | awk -F '=' '{print $2}'  )
+                sed -i "${line_number}s/${default_scale}/${scale}/" sig.conf 
+                sed -i "${line_number}s/${default_count}/${count}/" sig.conf
+                
                 echo "sig.conf file has been changed"
+                logger "sig.conf file has been changed" -t SigScript 
             else
                 echo "your selected inputs are not present in the sig.conf file"
+                logger "your selected inputs are not present in the sig.conf file" -t SigScript 
             fi
     else
-        if cat sig.conf | grep -n  "vdopiasample-bid" | grep -n -q "$COMPONENT_NAME";
+        if cat sig.conf | grep -n  "vdopiasample-bid" | grep -n -q "$component";
         then
-            LINE_NUMBER=$( cat sig.conf | grep -n "vdopiasample-bid" | grep  "$COMPONENT_NAME" | awk -F ':' '{print $1}')
-            DEFAULT_SCALE=$( cat sig.conf | grep -n "vdopiasample-bid" | grep -n "$COMPONENT_NAME" | awk -F ';' '{print $2}'  )
-            DEFAULT_COUNT=$( cat sig.conf | grep -n  "vdopiasample-bid" | grep -n "$COMPONENT_NAME" | awk -F '=' '{print $2}'  )
-            sed -i "${LINE_NUMBER}s/${DEFAULT_SCALE}/${SCALE}/" sig.conf
-            sed -i "${LINE_NUMBER}s/${DEFAULT_COUNT}/${COUNT}/" sig.conf
-
-            echo "sig.conf file has been changed"
+            line_number=$( cat sig.conf | grep -n "vdopiasample-bid" | grep  "$component" | awk -F ':' '{print $1}')
+            default_scale=$( cat sig.conf | grep -n "vdopiasample-bid" | grep -n "$component" | awk -F ';' '{print $2}'  )
+            default_count=$( cat sig.conf | grep -n  "vdopiasample-bid" | grep -n "$component" | awk -F '=' '{print $2}'  )
+            sed -i "${line_number}s/${default_scale}/${scale}/" sig.conf
+            sed -i "${line_number}s/${default_count}/${count}/" sig.conf
+            
+            echo  "sig.conf file has been changed"
+            logger "sig.conf file has been changed" -t SigScript 
         else
-              echo "your selected inputs are not present in the sig.conf file"  
+            echo  "your selected inputs are not present in the sig.conf file"
+            logger "your selected inputs are not present in the sig.conf file" -t SigScript 
         fi
 fi
+
