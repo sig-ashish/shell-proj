a4ecaebce14d3889bc5c68b902b0438fd8ca8da8
Ashish Yadav <95202504+sig-ashish@users.noreply.github.com>
2021-12-21 19:24:51 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,91 +1,36 @@
-#!/bin/bash
+
+!/bin/bash
 
 
-
-SELECT_COMPONENT_NAME=(INGESTOR JOINER WRANGLER VALIDATOR)
-SELECT_VIEW=(Auction Bid)
-SELECT_SCALE_VAR=(LOW MID HIGH)
-SELECT_COUNT=({1..9})
-
-
-
-# calling the function and storing the users value
-COMPONENT_NAME=$1
-VIEW=$2
-SCALE_VAR=$3
+comp_read=$1                                                    
+view_read=$2                                                
+scale_read=$3                                               
 COUNT=$4
-value=$5
+filename=$5
 
 
-
-
-if [[ " ${SELECT_COMPONENT_NAME[*]} " =~ " $COMPONENT_NAME " ]]; then
-        echo "Selected Component $COMPONENT_NAME"
-    else
-        echo "Wrong input: $COMPONENT_NAME"
-        exit
-fi
-
-        
-if [[ " ${SELECT_VIEW[*]} " =~ " $VIEW" ]]; then
-        echo "Selected Component $VIEW"
-    else
-        echo "Wrong input: $VIEW"
-        exit
-fi
-
-
-if [[ " ${SELECT_SCALE_VAR[*]} " =~ " $SCALE_VAR" ]]; then
-        echo "Selected Component $SCALE_VAR"
-    else
-        echo "Wrong input: $SCALE_VAR"
-        exit
-fi
-
-
-if [[ " ${SELECT_COUNT[*]} " =~ " $COUNT" ]]; then
-        echo "Selected Component $COUNT"
-    else
-        echo "Wrong input: $COUNT"
-        exit
-fi
-
-
-
-# matching the parameters and changing the content of file according to that
-if [ "$VIEW" == "Auction" ]; then
+VAR="bid"
+if [[ "$view_read" =~ "Auction" ]]; then
     
+    LineNumber=$( cat $filename | grep -i -n  "$comp_read" | grep -i -n -v "$VAR" | awk -F ":" '{print $2}' ) 
+    conf=$( cat $filename | grep -i  "$comp_read" | grep -i -v "$VAR"  )
+    SCALE=$( echo $conf | awk -F ';' '{print $2}')                            
+    SELECT_COUNT=$( echo $conf | awk -F '=' '{print $2}')         
+    new_conf=$( echo $conf | sed  "s/$SCALE/$scale_read/g"  )
 
-    if cat sig.conf | grep -n -v "$value" | grep -n -q "$COMPONENT_NAME" ; then
-        L=$( cat sig.conf | grep -n -v "$value" | grep  "$COMPONENT_NAME" | awk -F ':' '{print $1}')
-        PREVIOUS_SCALE=$( cat sig.conf | grep -n -v "$value" | grep -n "$COMPONENT_NAME" | awk -F ';' '{print $2}'  )
-        PREVIOUS_COUNT=$( cat sig.conf | grep -n -v "$value" | grep -n "$COMPONENT_NAME" | awk -F '=' '{print $2}'  )
-        sed -i "${L}s/${PREVIOUS_SCALE}/${SCALE_VAR}/" sig.conf 
-        sed -i "${L}s/${PREVIOUS_COUNT}/${COUNT}/" sig.conf
-        echo "sig.conf is changed now, check line number $L"
-    else
-        echo "Your select view and component name is not present in sig.conf file. Please try again"
-    fi
-
+    new_conf2=$( echo $new_conf | sed  "s/$SELECT_COUNT/$COUNT/g"  )
+    sed -i "s/$conf/$new_conf2/g" $filename
 
 else
+    LineNumber=$( cat $filename | grep -i -n "$comp_read" | grep -i -n "$VAR" | awk -F ":" '{print $2}' ) # conf
+    conf=$( cat $filename | grep -i  "$comp_read" | grep -i  "$VAR"  )
+    SCALE=$( echo $conf | awk -F ';' '{print $2}')                             # curr_scale 
+    SELECT_COUNT=$( echo $conf | awk -F '=' '{print $2}')           # new_conf
+    new_conf=$( echo $conf | sed  "s/$SCALE/$scale_read/g"  )
 
-    if cat sig.conf | grep -n  "$value" | grep -n -q "$COMPONENT_NAME"; then
-        L=$( cat sig.conf | grep -n "$value" | grep  "$COMPONENT_NAME" | awk -F ':' '{print $1}')
-        PREVIOUS_SCALE=$( cat sig.conf | grep -n "$value" | grep -n "$COMPONENT_NAME" | awk -F ';' '{print $2}'  )
-        PREVIOUS_COUNT=$( cat sig.conf | grep -n  "$value" | grep -n "$COMPONENT_NAME" | awk -F '=' '{print $2}'  ) 
-        sed -i "${L}s/${PREVIOUS_SCALE}/${SCALE_VAR}/" sig.conf
-        sed -i "${L}s/${PREVIOUS_COUNT}/${COUNT}/" sig.conf
-        echo  "sig.conf is changed now, check line number $L"
-    else
-        echo  "Your select view and component name is not present in sig.conf file. Please try again"
-    fi
+    new_conf2=$( echo $new_conf | sed  "s/$SELECT_COUNT/$COUNT/g"  )
+    sed -i "s/$conf/$new_conf2/g" $filename
 fi
+cat $filename
+~                                  
 
-
-# script - end
-
-echo ""
-echo "==============================================================================================================================="
-echo ""
-cat sig.conf
