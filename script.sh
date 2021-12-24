52bc2723f0abbc4f43baf406618777b423124c98
Ashish Yadav <95202504+sig-ashish@users.noreply.github.com>
2021-12-24 21:32:51 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,25 +11,33 @@ filename=$5
 
 VAR="bid"
 if [[ "$view_read" =~ "Auction" ]]; then
+        LineNumber=$( cat $filename | grep -i -n  "$comp_read" | grep -i -n -v "$VAR" | awk -F ":" '{print $2}' ) 
+        if [[ $LineNumber -ne "NULL" ]]; then
+            conf=$( cat $filename | grep -i  "$comp_read" | grep -i -v "$VAR"  )
+            SCALE=$( echo $conf | awk -F ';' '{print $2}')                            
+            SELECT_COUNT=$( echo $conf | awk -F '=' '{print $2}')         
+            new_conf=$( echo $conf | sed  "s/$SCALE/$scale_read/g"  )
     
-    LineNumber=$( cat $filename | grep -i -n  "$comp_read" | grep -i -n -v "$VAR" | awk -F ":" '{print $2}' ) 
-    conf=$( cat $filename | grep -i  "$comp_read" | grep -i -v "$VAR"  )
-    SCALE=$( echo $conf | awk -F ';' '{print $2}')                            
-    SELECT_COUNT=$( echo $conf | awk -F '=' '{print $2}')         
-    new_conf=$( echo $conf | sed  "s/$SCALE/$scale_read/g"  )
-
-    new_conf2=$( echo $new_conf | sed  "s/$SELECT_COUNT/$COUNT/g"  )
-    sed -i "s/$conf/$new_conf2/g" $filename
+            new_conf2=$( echo $new_conf | sed  "s/$SELECT_COUNT/$COUNT/g"  )
+            sed -i "s/$conf/$new_conf2/g" $filename
+       else
+            echo "content not found"
+       fi
 
 else
     LineNumber=$( cat $filename | grep -i -n "$comp_read" | grep -i -n "$VAR" | awk -F ":" '{print $2}' ) # conf
-    conf=$( cat $filename | grep -i  "$comp_read" | grep -i  "$VAR"  )
-    SCALE=$( echo $conf | awk -F ';' '{print $2}')                             # curr_scale 
-    SELECT_COUNT=$( echo $conf | awk -F '=' '{print $2}')           # new_conf
-    new_conf=$( echo $conf | sed  "s/$SCALE/$scale_read/g"  )
+    if [[ $LineNumber -ne "NULL" ]]; then
+    
+            conf=$( cat $filename | grep -i  "$comp_read" | grep -i  "$VAR"  )
+            SCALE=$( echo $conf | awk -F ';' '{print $2}')                             # curr_scale 
+            SELECT_COUNT=$( echo $conf | awk -F '=' '{print $2}')           # new_conf
+            new_conf=$( echo $conf | sed  "s/$SCALE/$scale_read/g"  )
 
-    new_conf2=$( echo $new_conf | sed  "s/$SELECT_COUNT/$COUNT/g"  )
-    sed -i "s/$conf/$new_conf2/g" $filename
+            new_conf2=$( echo $new_conf | sed  "s/$SELECT_COUNT/$COUNT/g"  )
+            sed -i "s/$conf/$new_conf2/g" $filename
+     else
+            echo "content not found"
+     fi
 fi
 cat $filename
 ~                                  
